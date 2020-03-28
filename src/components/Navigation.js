f56d36ccef714da253f5f8b8f8c5c3bcaa152db4
Kelly Silva <kelly@kellyregina.com.br>
2020-03-27 23:09:57 -0300

    add styled components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
+import css from 'styled-components'
+
+const Nav = css.nav`
+    background: #00E96F;
+`;
+
+const Svg = css.svg`
+    fill: #000000;
+`;
+
 const Navigation = () => (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <span className="font-semibold text-xl tracking-tight">Orbita</span>
-        </div>
-        <div className="block lg:hidden">
-            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    <Nav className="flex items-center justify-between flex-wrap p-6">
+        <div className="block">
+            <button className="flex items-center px-3 py-2 text-teal-200 border-teal-400 hover:text-white">
+                <Svg className="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                </Svg>
             </button>
         </div>
-    </nav>
+    </Nav>
 );
 
 export default Navigation;
